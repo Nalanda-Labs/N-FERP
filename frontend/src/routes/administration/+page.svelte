34aa0207c91e343ef150b955293bd54c9c6ccc4b
partha <pmondal095@gmail.com>
2023-09-19 19:17:47 +0530

    removed redundant code and fixed indentation

--- frontend/src/routes/administration/+page.svelte
+++ frontend/src/routes/administration/+page.svelte
@@ -18,66 +18,6 @@ Aministration
 
 <main class="bg-gray-100 h-screen overflow-hidden relative">
 	<div class="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-3 gap-4 my-4">
-		<!-- <div class="w-full">
-			<div class="shadow-lg px-4 py-6 w-full bg-white relative">
-				<p
-					class="text-sm w-max text-gray-700 font-semibold border-b border-gray-200"
-				>
-					Users And Authentication
-				</p>
-				<div class="flex items-end space-x-2 my-6">					
-				</div>
-				<div>
-					<div
-						class="flex items-center pb-2 mb-2 text-sm sm:space-x-12  justify-between border-b border-gray-200"
-					>
-                        <a href="/#">
-                            <p>User Management</p>
-                        </a>
-					</div>
-
-					<div
-						class="flex items-center mb-2 pb-2 text-sm space-x-12 md:space-x-24 justify-between border-b border-gray-200"
-					>
-                        <a href="/#">
-                            <p>Role Management</p>
-                        </a>		
-					</div>
-					<div class="lex items-center mb-2 pb-2 text-sm space-x-12 md:space-x-24 justify-between border-b border-gray-200">
-                        <a href="/#">
-                            <p>Password Management</p>
-                        </a>	
-					</div>
-
-                    <div class="lex items-center mb-2 pb-2 text-sm space-x-12 md:space-x-24 justify-between border-b border-gray-200">
-                        <a href="/#">
-                            <p>OAuth2 Clients and Tokens</p>
-                        </a>
-					</div>
-
-                    <div class="lex items-center mb-2 pb-2 text-sm space-x-12 md:space-x-24 justify-between border-b border-gray-200">	
-                        <a href="/#">
-                            <p>OAuth Keys</p>
-                        </a> 
-					</div>
-
-                    <div class="lex items-center mb-2 pb-2 text-sm space-x-12 md:space-x-24 justify-between border-b border-gray-200">	
-                        <a href="/#">
-                            <p>Security Suite Group Management</p>
-                        </a>
-					</div>
-
-                    <div class="lex items-center mb-2 pb-2 text-sm space-x-12 md:space-x-24 justify-between border-b border-gray-200">
-                        <a href="/#">
-                            <p>Security Suite Settings</p>
-                        </a>	
-					</div>
-                    
-
-                    
-				</div>
-			</div>
-		</div> -->
         {#each list as content}
 		<div>
 			<div class="shadow-lg px-4 py-6 w-full bg-white relative">
--- frontend/src/routes/administration/users/+page.svelte
+++ frontend/src/routes/administration/users/+page.svelte
@@ -1,7 +1,3 @@
-<script>
- 
- 
-</script>
 
 USERS
 
@@ -11,181 +7,179 @@ USERS
 	</div>
 
 	<div class="py-8">
-		
-			<div class="max-w-full overflow-x-auto rounded-lg">
-				<table class="table-auto leading-normal text-black">
-					<thead>
-						<tr>
+		<div class="max-w-full overflow-x-auto rounded-lg">
+			<table class="table-auto leading-normal text-black">
+				<thead>
+					<tr>
+						<th
+							scope="col"
+							class="font-extrabold border-b border-gray-200 px-5 py-3 text-justify text-sm uppercase"
+						>
+							First Name
+						</th>
+						<th
+							scope="col"
+							class=" font-extrabold border-b border-gray-200 px-5 py-3 text-justify text-sm uppercase"
+						>
+							Last Name
+						</th>
+						<th
+							scope="col"
+							class="font-extrabold border-b border-gray-200 px-5 py-3 text-justify text-sm uppercase"
+						>
+							User Name
+							
+						</th>
+						<th
+							scope="col"
+							class="font-extrabold border-b border-gray-200 px-5 py-3 text-justify text-sm uppercase"
+						>
+							Status
+						</th>
+						<th
+							scope="col"
+							class="font-extrabold border-b border-gray-200 px-5 py-3 text-justify text-sm uppercase"
+						>
+							Phone
+						</th>
+						<th
+							scope="col"
+							class="font-extrabold border-b border-gray-200 px-5 py-3 text-justify text-sm uppercase"
+						>
+							Email
+						</th>
+						<th
+							scope="col"
+							class="font-extrabold border-b border-gray-200 px-5 py-3 text-justify text-sm uppercase"
+						>
+							Role
+						</th>
+						<th
+							scope="col"
+							class="font-extrabold border-b border-gray-200 px-5 py-3 text-justify text-sm uppercase"
+						>
+							Department
+						</th>
 							<th
-								scope="col"
-								class="font-extrabold border-b border-gray-200 px-5 py-3 text-justify text-sm uppercase"
-							>
-								First Name
-							</th>
-							<th
-								scope="col"
-								class=" font-extrabold border-b border-gray-200 px-5 py-3 text-justify text-sm uppercase"
-							>
-								Last Name
-							</th>
-							<th
-								scope="col"
-								class="font-extrabold border-b border-gray-200 px-5 py-3 text-justify text-sm uppercase"
-							>
-								User Name
-								
-							</th>
-							<th
-								scope="col"
-								class="font-extrabold border-b border-gray-200 px-5 py-3 text-justify text-sm uppercase"
-							>
-								Status
-							</th>
-							<th
-								scope="col"
-								class="font-extrabold border-b border-gray-200 px-5 py-3 text-justify text-sm uppercase"
-							>
-								Phone
-							</th>
-							<th
-								scope="col"
-								class="font-extrabold border-b border-gray-200 px-5 py-3 text-justify text-sm uppercase"
-							>
-								Email
-							</th>
-							<th
-								scope="col"
-								class="font-extrabold border-b border-gray-200 px-5 py-3 text-justify text-sm uppercase"
-							>
-								Role
-							</th>
-							<th
-								scope="col"
-								class="font-extrabold border-b border-gray-200 px-5 py-3 text-justify text-sm uppercase"
-							>
-								Department
-							</th>
-								<th
-								scope="col"
-								class="font-extrabold border-b border-gray-200 px-5 py-3 text-justify text-sm uppercase"
-							>
-								System Administrator
-							</th>
-							<th
-								scope="col"
-								class="font-extrabold border-b border-gray-200 px-5 py-3 text-left text-sm uppercase"
-							/>
-						</tr>
-					</thead>
-					<tbody class="text-black">
-						<tr>
-							<td class="border-b border-gray-200 p-5 text-sm">
-								<div class="flex items-center">
-									<div class="shrink-0">
-										<span class="relative block">
-											<img
-												alt="profil"
-												src="https://res.cloudinary.com/beloved/image/upload/v1623196231/Assets/person/8_yfarwk.jpg"
-												class="mx-auto h-10 w-10 rounded-full object-cover"
-											/>
-										</span>
-									</div>
-									<div class="ml-3">
-										<p class="whitespace-nowrap text-justify">Shiv</p>
-									</div>
+							scope="col"
+							class="font-extrabold border-b border-gray-200 px-5 py-3 text-justify text-sm uppercase"
+						>
+							System Administrator
+						</th>
+						<th
+							scope="col"
+							class="font-extrabold border-b border-gray-200 px-5 py-3 text-left text-sm uppercase"
+						/>
+					</tr>
+				</thead>
+				<tbody class="text-black">
+					<tr>
+						<td class="border-b border-gray-200 p-5 text-sm">
+							<div class="flex items-center">
+								<div class="shrink-0">
+									<span class="relative block">
+										<img
+											alt="profil"
+											src="https://res.cloudinary.com/beloved/image/upload/v1623196231/Assets/person/8_yfarwk.jpg"
+											class="mx-auto h-10 w-10 rounded-full object-cover"
+										/>
+									</span>
 								</div>
-							</td>
-							<td class="border-b border-gray-200 p-5 text-sm">
-								<p class="whitespace-nowrap text-justify">Dayal</p>
-							</td>
-							<td class="border-b border-gray-200 p-5 text-sm">
-								<p class="whitespace-nowrap text-justify">ShivD</p>
-							</td>
-							<td class="border-b border-gray-200 p-5 text-sm">
-								<span class="relative inline-block px-3 py-1 font-semibold leading-tight">
-									<span
-										aria-hidden="true"
-										class="absolute inset-0 rounded-full bg-green-200 opacity-50"
-									/>
-									<span class="relative whitespace-nowrap text-justify">active</span>
-								</span>
-							</td>
-								<td class="border-b border-gray-200 p-5 text-sm">
-								<p class="whitespace-nowrap text-justify">12345678910</p>
-							</td>
-								<td class="border-b border-gray-200 p-5 text-sm">
-								<p class="whitespace-nowrap text-justify">shivdayal@gmail.com</p>
-							</td>
-								<td class="border-b border-gray-200 p-5 text-sm">
-								<p class="whitespace-nowrap text-justify">Admin</p>
-							</td>
-								<td class="border-b border-gray-200 p-5 text-sm">
-								<p class="whitespace-nowrap text-justify">Technology</p>
-							</td>
-								<td class="border-b border-gray-200 p-5 text-sm">
-								<p class="whitespace-nowrap text-justify">Yes</p>
-							</td>
-							<td class="border-b border-gray-200 p-5 text-sm">
-								<span class="text-indigo-600 hover:text-indigo-900"> Edit </span>
-							</td>
-						</tr>
-						<tr>
-							<td class="border-b border-gray-200 p-5 text-sm">
-								<div class="flex items-center">
-									<div class="shrink-0">
-										<span class="relative block">
-											<img
-												alt="profil"
-												src="https://res.cloudinary.com/beloved/image/upload/v1623196231/Assets/person/4_iauuag.jpg"
-												class="mx-auto h-10 w-10 rounded-full object-cover"
-											/>
-										</span>
-									</div>
-									<div class="ml-3">
-										<p class="whitespace-nowrap">Partha</p>
-									</div>
+								<div class="ml-3">
+									<p class="whitespace-nowrap text-justify">Shiv</p>
 								</div>
-							</td>
-							<td class="border-b border-gray-200 p-5 text-sm">
-								<p class="whitespace-nowrap">Mondal</p>
-							</td>
-							<td class="border-b border-gray-200 p-5 text-sm">
-								<p class="whitespace-nowrap">pmondal095</p>
-							</td>
-							<td class="border-b border-gray-200 p-5 text-sm">
+							</div>
+						</td>
+						<td class="border-b border-gray-200 p-5 text-sm">
+							<p class="whitespace-nowrap text-justify">Dayal</p>
+						</td>
+						<td class="border-b border-gray-200 p-5 text-sm">
+							<p class="whitespace-nowrap text-justify">ShivD</p>
+						</td>
+						<td class="border-b border-gray-200 p-5 text-sm">
+							<span class="relative inline-block px-3 py-1 font-semibold leading-tight">
 								<span
-									class="relative inline-block px-3 py-1 font-semibold leading-tight text-green-900"
-								>
-									<span
-										aria-hidden="true"
-										class="absolute inset-0 rounded-full bg-green-200 opacity-50"
-									/>
-									<span class="relative">active</span>
-								</span>
-							</td>
+									aria-hidden="true"
+									class="absolute inset-0 rounded-full bg-green-200 opacity-50"
+								/>
+								<span class="relative whitespace-nowrap text-justify">active</span>
+							</span>
+						</td>
 							<td class="border-b border-gray-200 p-5 text-sm">
-								<p class="whitespace-nowrap">9830563278</p>
-							</td>
+							<p class="whitespace-nowrap text-justify">12345678910</p>
+						</td>
 							<td class="border-b border-gray-200 p-5 text-sm">
-								<p class="whitespace-nowrap">pmondal095@gmail.com</p>
-							</td>
-								<td class="border-b border-gray-200 p-5 text-sm">
-								<p class="whitespace-nowrap">Developer</p>
-							</td>
-								<td class="border-b border-gray-200 p-5 text-sm">
-								<p class="whitespace-nowrap">Technology</p>
-							</td>
-								<td class="border-b border-gray-200 p-5 text-sm">
-								<p class="whitespace-nowrap">No</p>
-							</td>
+							<p class="whitespace-nowrap text-justify">shivdayal@gmail.com</p>
+						</td>
 							<td class="border-b border-gray-200 p-5 text-sm">
-								<span class="text-indigo-600 hover:text-indigo-900"> Edit </span>
-							</td>
-		
-					</tbody>
-				</table>
-			</div>
-		
+							<p class="whitespace-nowrap text-justify">Admin</p>
+						</td>
+							<td class="border-b border-gray-200 p-5 text-sm">
+							<p class="whitespace-nowrap text-justify">Technology</p>
+						</td>
+							<td class="border-b border-gray-200 p-5 text-sm">
+							<p class="whitespace-nowrap text-justify">Yes</p>
+						</td>
+						<td class="border-b border-gray-200 p-5 text-sm">
+							<span class="text-indigo-600 hover:text-indigo-900"> Edit </span>
+						</td>
+					</tr>
+					<tr>
+						<td class="border-b border-gray-200 p-5 text-sm">
+							<div class="flex items-center">
+								<div class="shrink-0">
+									<span class="relative block">
+										<img
+											alt="profil"
+											src="https://res.cloudinary.com/beloved/image/upload/v1623196231/Assets/person/4_iauuag.jpg"
+											class="mx-auto h-10 w-10 rounded-full object-cover"
+										/>
+									</span>
+								</div>
+								<div class="ml-3">
+									<p class="whitespace-nowrap">Partha</p>
+								</div>
+							</div>
+						</td>
+						<td class="border-b border-gray-200 p-5 text-sm">
+							<p class="whitespace-nowrap">Mondal</p>
+						</td>
+						<td class="border-b border-gray-200 p-5 text-sm">
+							<p class="whitespace-nowrap">pmondal095</p>
+						</td>
+						<td class="border-b border-gray-200 p-5 text-sm">
+							<span
+								class="relative inline-block px-3 py-1 font-semibold leading-tight text-green-900"
+							>
+								<span
+									aria-hidden="true"
+									class="absolute inset-0 rounded-full bg-green-200 opacity-50"
+								/>
+								<span class="relative">active</span>
+							</span>
+						</td>
+						<td class="border-b border-gray-200 p-5 text-sm">
+							<p class="whitespace-nowrap">9830563278</p>
+						</td>
+						<td class="border-b border-gray-200 p-5 text-sm">
+							<p class="whitespace-nowrap">pmondal095@gmail.com</p>
+						</td>
+							<td class="border-b border-gray-200 p-5 text-sm">
+							<p class="whitespace-nowrap">Developer</p>
+						</td>
+							<td class="border-b border-gray-200 p-5 text-sm">
+							<p class="whitespace-nowrap">Technology</p>
+						</td>
+							<td class="border-b border-gray-200 p-5 text-sm">
+							<p class="whitespace-nowrap">No</p>
+						</td>
+						<td class="border-b border-gray-200 p-5 text-sm">
+							<span class="text-indigo-600 hover:text-indigo-900"> Edit </span>
+						</td>
+	
+				</tbody>
+			</table>
+		</div>
 	</div>
 
 	</div>
